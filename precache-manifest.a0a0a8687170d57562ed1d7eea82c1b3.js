388b3ec8ae65c2e5480b12d39119a3a5db50b7fa
Alexiz <qwastanja@hotmail.com>
2020-09-11 16:44:32 +0200

    Updates

---
+++ precache-manifest.a0a0a8687170d57562ed1d7eea82c1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4a31b042d2c2452a1ad93602f5c4b7",
+    "url": "/loldex/index.html"
+  },
+  {
+    "revision": "b681acd6c864223ff54f",
+    "url": "/loldex/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3b6479826c5c44457ce6",
+    "url": "/loldex/static/css/main.749f7275.chunk.css"
+  },
+  {
+    "revision": "b681acd6c864223ff54f",
+    "url": "/loldex/static/js/2.97a65e4d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/loldex/static/js/2.97a65e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6479826c5c44457ce6",
+    "url": "/loldex/static/js/main.2af8576a.chunk.js"
+  },
+  {
+    "revision": "3db0acbdc762e3c5ed58",
+    "url": "/loldex/static/js/runtime-main.8bf0293e.js"
+  }
+]);
