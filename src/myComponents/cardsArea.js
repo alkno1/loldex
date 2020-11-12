9d405204d007fb259f47f94f141522f405d02de5
Alexiz Knöös <qwastanja@hotmail.com>
2020-11-12 20:35:02 +0100

    Update cardsArea.js

--- src/myComponents/cardsArea.js
+++ src/myComponents/cardsArea.js
@@ -2,7 +2,6 @@ import React from "react";
 import ChampionCard from "./championCard";
 
 function CardsArea(props) {
-  //championListToShow ska vara länken till de olika listorna till ex tagsen i filter med ex if-sats
   const championCards = props.championListToShow.map((champion) => (
     <div key={champion}>
       <ChampionCard
