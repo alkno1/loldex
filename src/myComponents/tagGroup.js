37fe87449e5089cc89d3ef611846694985065e87
Alexiz Knöös <qwastanja@hotmail.com>
2020-11-12 20:40:25 +0100

    Update tagGroup.js

--- src/myComponents/tagGroup.js
+++ src/myComponents/tagGroup.js
@@ -2,8 +2,6 @@ import React from "react";
 import Badge from "react-bootstrap/Badge";
 import Spacer from "./spacer";
 
-//Loopa alla taggs för aktuell champ
-
 function TagGroup(props) {
   const championTagBadges = props.generalChampionsInfo.data[
     props.champion
